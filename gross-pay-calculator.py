9637c749b92ffa537290097634c7bf115dd1be67
thebiparadox <swehlatessa@gmail.com>
2024-08-02 11:33:16 -0400

    Refactor rewrote code to give employees 1.5 times the hourly rate for hours worked above 40.
    rewrote code to catch user imput exceptions

--- gross-pay-calculator.py
+++ gross-pay-calculator.py
@@ -1,4 +1,21 @@
-hours = input('Enter Hours: ') #user inputs hours and rate
-rate = input('Enter Rate: ')
-pay = float(hours) * float(rate) #converts input str to float and multiplies hours and rate to get pay
+import sys
+
+hoursInput = input('Enter Hours: ') #user inputs hours and rate
+try:
+    hours = float(hoursInput)
+except:
+    print('Error, please enter numeric input')
+    sys.exit()
+rateInput = input('Enter Rate: ')
+try:
+    rate = float(rateInput)
+except:
+    print('Error, please enter numeric input')
+    sys.exit()
+
+if hours <= 40.0: #calcuates rate under 40 hours worked
+    pay = hours * rate
+elif hours > 40.0: #calculates rate over 40 hours worked
+    pay = hours * (rate * 1.5)
+
 print("Pay:", round(pay)) #rounds to the nearest integer and prints result
