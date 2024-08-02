be0ecab7a7cb9f4ed81d2b3d1f47fa89712582d8
thebiparadox <swehlatessa@gmail.com>
2024-08-02 12:00:41 -0400

    refactored

--- gross-pay-calculator.py
+++ gross-pay-calculator.py
@@ -1,4 +1,4 @@
-import sys
+import sys #gets sys module
 
 hoursInput = input('Enter Hours: ') #user inputs hours and rate
 try:
@@ -16,6 +16,9 @@ except:
 if hours <= 40.0: #calcuates rate under 40 hours worked
     pay = hours * rate
 elif hours > 40.0: #calculates rate over 40 hours worked
-    pay = hours * (rate * 1.5)
+    regularPay = 40.0 * rate
+    overtime = hours - 40
+    overtimePay = overtime * (rate * 1.5)
+    pay = regularPay + overtimePay
 
 print("Pay:", round(pay)) #rounds to the nearest integer and prints result
