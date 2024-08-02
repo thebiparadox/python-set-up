fa8f960198512ab8630ae39c52156eef9f13f2da
thebiparadox <swehlatessa@gmail.com>
2024-08-02 12:00:06 -0400

    refactored

---
+++ grader.py
@@ -0,0 +1,22 @@
+import sys #gets sys module
+
+scoreInput = input('Please input your score here: ') #gets score from user
+
+try: #catches any user exceptions
+    score = float(scoreInput)
+except:
+    print('Bad score')
+    sys.exit()
+
+if score > 1.0 or score < 0.0:
+    print('Bad score')
+elif score >= 0.9:
+    print('A')
+elif score >= 0.8:
+    print('B')
+elif score >= 0.7:
+    print('C')
+elif score >= 0.6:
+    print('D')
+else:
+    print('F')
