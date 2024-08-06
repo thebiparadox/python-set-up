7d578b06ac76e2974536a62a4e81ae93708a05e1
thebiparadox <swehlatessa@gmail.com>
2024-08-06 10:48:49 -0400

    wrapped computation in function

--- grader.py
+++ grader.py
@@ -8,15 +8,18 @@ except:
     print('Bad score')
     sys.exit()
 
-if score > 1.0 or score < 0.0:
-    print('Bad score')
-elif score >= 0.9:
-    print('A')
-elif score >= 0.8:
-    print('B')
-elif score >= 0.7:
-    print('C')
-elif score >= 0.6:
-    print('D')
-else:
-    print('F')
+def computeGrade(score): #computes and prints grade
+    if score > 1.0 or score < 0.0:
+        print('Bad score')
+    elif score >= 0.9:
+        print('A')
+    elif score >= 0.8:
+        print('B')
+    elif score >= 0.7:
+        print('C')
+    elif score >= 0.6:
+        print('D')
+    else:
+        print('F')
+
+computeGrade(score)
