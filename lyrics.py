6926abda884441375e5e51a45ef4319e75532312
thebiparadox <swehlatessa@gmail.com>
2024-08-06 10:24:20 -0400

    Created lyrics.py to learn about functions

---
+++ lyrics.py
@@ -0,0 +1,11 @@
+
+
+def repeatLyrics(): #defines new function
+    printLyrics()
+    printLyrics()
+
+def printLyrics(): #defines function
+    print('Picture me in the weeds')
+    print('Before I learned civility, I used to scream any time I wanted')
+
+repeatLyrics() #calls second function
